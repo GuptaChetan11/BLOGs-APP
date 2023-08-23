a7b9da5b16c9e8db14efe75bae654c88b69853e7
Chetan <chetang1109@gmail.com>
2023-08-23 14:30:57 +0530

    added Blog functionalities

--- server.js
+++ server.js
@@ -10,28 +10,29 @@ dotenv.config();
 
 //router import
 const userRoutes = require("./routes/userRoutes");
+const blogRoutes = require("./routes/blogRoutes");
 
-//momgodb connection
+//mongodb connection
 connectDB();
 
-//rest object
+//rest objecct
 const app = express();
 
-//middleware
+//middelwares
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
 //routes
 app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/blog", blogRoutes);
 
-//port
+// Port
 const PORT = process.env.PORT || 8080;
-
 //listen
 app.listen(PORT, () => {
   console.log(
-    `Server Running on ${process.env.DEV_MODE} node port no ${PORT}`.bgCyan
+    `Server Running on ${process.env.DEV_MODE} mode port no ${PORT}`.bgCyan
       .white
   );
 });
