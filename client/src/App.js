afef66aead9528480bd45f142fbc08ecabdf92bf
Chetan <chetang1109@gmail.com>
2023-08-28 01:02:55 +0530

    fixed some bugs

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import Header from "./components/Header";
+import React  from 'react';
 import { Routes, Route } from "react-router-dom";
 import Blogs from "./pages/Blogs";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import UserBlogs from "./pages/UserBlogs";
-import CreateBlog from "./pages/CreateBlog";
-import BlogDetails from "./pages/BlogDetails";
+// import UserBlogs from "./pages/UserBlogs";
+// import CreateBlog from "./pages/CreateBlog";
+// // import BlogDetails from "./pages/BlogDetails";
 import { Toaster } from "react-hot-toast";
 function App() {
   return (
@@ -15,9 +16,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Blogs />} />
         <Route path="/blogs" element={<Blogs />} />
-        <Route path="/my-blogs" element={<UserBlogs />} />
-        <Route path="/blog-details/:id" element={<BlogDetails />} />
-        <Route path="/create-blog" element={<CreateBlog />} />
+        {/* <Route path="/my-blogs" element={<UserBlogs />} />
+        {/* <Route path="/blog-details/:id" element={<BlogDetails />} /> */}
+        {/* <Route path="/create-blog" element={<CreateBlog />} /> */}
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
